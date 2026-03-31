98a897e500c46d8f39efe871a0a55c94997fd3c7
Estefania <estefania.osses.v@gmail.com>
2026-03-31 03:08:09 -0300

    fix(footer): remove unused variable

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { FaDribbble, FaLinkedin, FaGithub, } from 'react-icons/fa'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 import { IoMdMail } from "react-icons/io";
 
 const Footer = () => {
